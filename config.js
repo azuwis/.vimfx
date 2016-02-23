59dddb588e64ea09e10890ae50ec15631dd5e102
Zhong Jianxin <azuwis@gmail.com>
2016-02-23 10:08:43 +0800

    Init my config

--- config.js
+++ config.js
@@ -1,4 +1,21 @@
-// This is the actual config file, written in JavaScript. It is in this file you
-// add custom commands and set options, or whatever you'd like to do.
+// example: https://github.com/lydell/dotfiles/blob/master/.vimfx/config.js
 
-console.log('Hello, world! This is vimfx:', vimfx)
+let map = (shortcuts, command, custom=false) => {
+    vimfx.set(`${custom ? 'custom.' : ''}mode.normal.${command}`, shortcuts)
+}
+
+map('', 'window_new')
+map('w', 'tab_select_previous')
+map('e', 'tab_select_next')
+
+let set = (pref, valueOrFunction) => {
+    let value = typeof valueOrFunction === 'function'
+        ? valueOrFunction(vimfx.getDefault(pref))
+        : valueOrFunction
+    vimfx.set(pref, value)
+}
+
+set('prevent_autofocus', true)
+set('hints_sleep', -1)
+set('prev_patterns', v => `[上前]\\s*一?\\s*[页张个篇章頁] newer\\s*posts? ${v}`)
+set('next_patterns', v => `[下后]\\s*一?\\s*[页张个篇章頁] older\\s*posts? ${v}`)
