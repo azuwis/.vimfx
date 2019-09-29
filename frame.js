d1ddd29df7dc1efd3e18499d36f083bc4ae761f0
Zhong Jianxin <azuwis@gmail.com>
2019-09-29 13:54:07 +0800

    Only listen on moz-extension:// does not work in this way
    When content page init, content.document.URL is not available yet

--- frame.js
+++ frame.js
@@ -12,40 +12,38 @@ vimfx.listen('getInfo', (data, callback) => {
     callback({title, url, selection, href})
 })
 
-if (content.document.URL.startsWith('moz-extension://') && content.document.URL.endsWith('#3p-filters.html')) {
-    vimfx.listen('ublockBootstrap', (data, callback) => {
-        let filters = {
-            'easyprivacy': true,
-            'easylist': true,
-            'CHN-0': true,
-            'CHN-1': true,
-            'https://github.com/azuwis/org/raw/master/adblock-filters.txt': true,
-        }
-        let {document} = content
-        document = document.querySelector('iframe').contentWindow.document
-        let lists = document.querySelectorAll('#lists li.listEntry:not(.unused)')
-        let handledFilters = []
-        for (let item of lists) {
-            let key = item.getAttribute('data-listkey')
-            if (filters.hasOwnProperty(key)) {
-                item.querySelector('input[type="checkbox"]').checked = filters[key]
-                if (key.startsWith('https://')) {
-                    handledFilters.push(key)
-                }
+vimfx.listen('ublockBootstrap', (data, callback) => {
+    let filters = {
+        'easyprivacy': true,
+        'easylist': true,
+        'CHN-0': true,
+        'CHN-1': true,
+        'https://github.com/azuwis/org/raw/master/adblock-filters.txt': true,
+    }
+    let {document} = content
+    document = document.querySelector('iframe').contentWindow.document
+    let lists = document.querySelectorAll('#lists li.listEntry:not(.unused)')
+    let handledFilters = []
+    for (let item of lists) {
+        let key = item.getAttribute('data-listkey')
+        if (filters.hasOwnProperty(key)) {
+            item.querySelector('input[type="checkbox"]').checked = filters[key]
+            if (key.startsWith('https://')) {
+                handledFilters.push(key)
             }
         }
-        let importCheckbox = document.querySelector('#importLists')
-        if (!importCheckbox.checked) {
-            importCheckbox.click()
-        }
-        let externalLists = document.querySelector('#externalLists')
-        let externalListsString = Object.keys(filters).filter(e => e.startsWith('https://') && !handledFilters.includes(e)).join("\n")
-        externalLists.value = externalListsString
-        let buttonApply = document.querySelector('#buttonApply')
-        buttonApply.classList.remove('disabled')
-        buttonApply.click()
-        // let buttonUpdate = document.querySelector('#buttonUpdate')
-        // buttonUpdate.classList.remove('disabled')
-        // buttonUpdate.click()
-    })
-}
+    }
+    let importCheckbox = document.querySelector('#importLists')
+    if (!importCheckbox.checked) {
+        importCheckbox.click()
+    }
+    let externalLists = document.querySelector('#externalLists')
+    let externalListsString = Object.keys(filters).filter(e => e.startsWith('https://') && !handledFilters.includes(e)).join("\n")
+    externalLists.value = externalListsString
+    let buttonApply = document.querySelector('#buttonApply')
+    buttonApply.classList.remove('disabled')
+    buttonApply.click()
+    // let buttonUpdate = document.querySelector('#buttonUpdate')
+    // buttonUpdate.classList.remove('disabled')
+    // buttonUpdate.click()
+})
