ed041ef518c40fa414d0cb3bcbc0dcb1d9857791
Zhong Jianxin <azuwis@gmail.com>
2019-09-29 10:42:06 +0800

    Combine filters and customFilters

--- frame.js
+++ frame.js
@@ -19,19 +19,19 @@ if (content.document.URL.startsWith('moz-extension://') && content.document.URL.
             'easylist': true,
             'CHN-0': true,
             'CHN-1': true,
+            'https://github.com/azuwis/org/raw/master/adblock-filters.txt': true,
         }
-        let customFilters = [
-            'https://github.com/azuwis/org/raw/master/adblock-filters.txt'
-        ]
         let {document} = content
         document = document.querySelector('iframe').contentWindow.document
         let lists = document.querySelectorAll('#lists li.listEntry:not(.unused)')
+        let handledFilters = []
         for (let item of lists) {
             let key = item.getAttribute('data-listkey')
-            let value = filters[key]
-            if (value) {
-                let checkbox = item.querySelector('input[type="checkbox"]')
-                checkbox.checked = value
+            if (filters.hasOwnProperty(key)) {
+                item.querySelector('input[type="checkbox"]').checked = filters[key]
+                if (key.startsWith('https://')) {
+                    handledCustomFilters.push(key)
+                }
             }
         }
         let importCheckbox = document.querySelector('#importLists')
@@ -39,15 +39,13 @@ if (content.document.URL.startsWith('moz-extension://') && content.document.URL.
             importCheckbox.click()
         }
         let externalLists = document.querySelector('#externalLists')
-        let customFiltersString = customFilters.join("\n")
-        if (externalLists.value !== customFiltersString) {
-            externalLists.value = customFiltersString
-        }
-        let button = document.querySelector('#buttonApply')
-        button.disabled = false
-        button.click()
-        button = document.querySelector('#buttonUpdate')
-        // button.classList.remove('disabled')
-        button.click()
+        let externalListsString = Object.keys(filters).filter(e => e.startsWith('https://') && !handledFilters.includes(e)).join("\n")
+        externalLists.value = externalListsString
+        let buttonApply = document.querySelector('#buttonApply')
+        buttonApply.disabled = false
+        buttonApply.click()
+        let buttonUpdate = document.querySelector('#buttonUpdate')
+        // buttonUpdate.classList.remove('disabled')
+        buttonUpdate.click()
     })
 }
